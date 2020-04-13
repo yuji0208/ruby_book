95988f7852827a65eced4e1cd732850064f23412
yuji0208 <yuji2080jtcxs@icloud.com>
2020-04-13 22:04:35 +0900

    進捗状況

--- ruby4/array.rb
+++ ruby4/array.rb
@@ -1,11 +1,13 @@
-a = [1,2,3,4,5]
+# a = [1,2,3,4,5]
 # puts a[1, 3] #配列[インデックス番号, 取得する長さ（個数）]
 # puts a.values_at(0, 2, 4) #values_atメソッドで取得したい要素のインデックス番号を複数指定可能
 # puts a[a.size - 2,]
 # puts a.last #配列の最後の要素を取得
 # puts a.last(2)
-puts a.first
-puts a.first(2)
+# puts a.first
+# puts a.first(2)
+
+a = [1,2,3]
 
 #戻り値を配列のまま受け取る
 # quo_rem = 14.divmod(3)
